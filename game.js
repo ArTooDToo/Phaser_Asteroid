5573d625735e5595ddc0d275f725f9e8bc692e87
ArTooDToo <ArTooDToo@users.noreply.github.com>
2017-04-20 10:21:57 +0200

    Nice work so far
    Restart state when hit the enemy, better collision

--- game.js
+++ game.js
@@ -1,43 +1,51 @@
-var w_height, w_width, x1, y1;
+var w_height, w_width
 
 w_height = 400;
 w_width = 400;
-x1 = 25;
-y1 = 25;
+
 var bullet;
 var bullets;
 var graphics;
 var bulletTime = 0
-var bb, circle
+var smt, fireButton
 
 var game = new Phaser.Game(w_height, w_width, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render});
 
 function preload() {
   game.load.image("bb", "bullet.png")
   game.load.image("ship", "ship.png")
+  game.load.image("smt", "smt.png")
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-graphics = game.add.sprite(200,200, 'ship');
-//nie działa wraz z generowanym tłem
-//game.renderer.clearBeforeRender = false;
-  game.renderer.roundPixels = true;
+  //nie działa wraz z generowanym tłem
+  //game.renderer.clearBeforeRender = false;
+  //game.renderer.roundPixels = true;
 
   bullets = game.add.group();
   bullets.enableBody = true;
   bullets.physicsBodyType = Phaser.Physics.ARCADE;
-  bullets.createMultiple(40, "bb");
+  bullets.createMultiple(30, "bb");
   bullets.setAll('anchor.x', 0.5);
   bullets.setAll('anchor.y', 0.5);
 
 
+  graphics = game.add.sprite(200,200, 'ship');
   graphics.anchor.set(0.5)
-
   graphics.angle = -90
   game.physics.enable(graphics, Phaser.Physics.ARCADE);
-  graphics.body.collideWorldBounds = true;
+  graphics.body.collideWorldBounds = false;
+
+  smt = game.add.sprite(enemySpawnX(),enemySpawnY(), 'smt');
+  smt.enableBody = true;
+  game.physics.enable(smt, Phaser.Physics.ARCADE);
+
+
+
+
   cursors = game.input.keyboard.createCursorKeys();
+  fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
 
@@ -51,7 +59,7 @@ function fireBullet () {
         if (bullet)
         {
             bullet.reset(graphics.body.x +25, graphics.body.y + 25);
-            bullet.lifespan = 500;
+            bullet.lifespan = 2000;
             bullet.rotation = graphics.rotation;
             game.physics.arcade.velocityFromRotation(graphics.rotation,400, bullet.body.velocity);
             bulletTime = game.time.now + 50;
@@ -62,9 +70,9 @@ function fireBullet () {
 
 
 function update() {
-
   if (cursors.left.isDown ){
         graphics.body.angularVelocity = -300;
+
   }else if (cursors.right.isDown){
         graphics.body.angularVelocity = 300;
   }else{
@@ -75,12 +83,20 @@ function update() {
   }else{
     graphics.body.acceleration.set(0);
   }
-  if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+  if(fireButton.isDown){
+
       fireBullet();
   }
   screenWrap(graphics)
-  bullets.forEachExists(screenWrap, this);
+  //bullets.forEachExists(screenWrap, this);
+  game.physics.arcade.overlap(bullets, smt, bulletHit, null, this);
 
+}
+
+function bulletHit(_bullet, _smt) {
+  _smt.kill()
+  _bullet.kill()
+  game.state.start(game.state.current)
 }
 
 function screenWrap(sprite) {
@@ -95,8 +111,18 @@ function screenWrap(sprite) {
   }
 }
 
+function enemySpawnX() {
+  var randomX = game.rnd.integerInRange(0,350)
+  return randomX
+}
+function enemySpawnY() {
+  var randomY = game.rnd.integerInRange(0,350)
+  return randomY
+}
+
 function render() {
     game.debug.spriteInfo(graphics, 32, 32);
+    game.debug.body(smt);
     game.debug.body(graphics);
 
 }
